efa91a72f11d41e116c0c6d663a0f15cb19a216a
Shyam792004 <shyammakesh79@gmail.com>
2024-03-14 11:01:33 +0530

    slogin updated

---
+++ parikchitra/src/assets/student/components/slogin.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './slogin.css';
+
+const SLogin = () => {
+    const [action, setAction] = useState("Login");
+
+    return (
+        <div className='container'>
+            {/* <div className="header">
+                <div className="text">{action}</div>
+                <div className="underline"></div>
+            </div> */}
+
+            <div className="submit-container">
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }} >Register</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+            </div>
+            <div className="inputs">
+                {action === "Login" ? <div></div> : <div className="input">
+                    <input type="text" placeholder="Username" />
+                </div>}
+                <div className="input">
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="input">
+                    <input type="text" placeholder="Email" />
+                </div>
+                <div className="input">
+                    <input type="text" placeholder="Password" />
+                </div>
+            </div>
+            <div className="lostpassword">
+                {action === "Sign Up" ? <div></div> : <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+            </div>
+        </div>
+    );
+};
+
+export default SLogin;
